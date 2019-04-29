2f3e73e457e5f6998003d2a1b67b7d5d4d141e57
Patryk Keska <patrykkeska@MacBook-Pro-MacbookPro.local>
2019-04-29 21:32:22 +0200

    gh

---
+++ build/precache-manifest.52e6f369d72693d96b852b2c3f3d111d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2d38ab6dc1f45e8eef0",
+    "url": "/weather-forecast/static/js/runtime~main.f2d38ab6.js"
+  },
+  {
+    "revision": "049db8f46def8c027c8d",
+    "url": "/weather-forecast/static/js/main.049db8f4.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/weather-forecast/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "049db8f46def8c027c8d",
+    "url": "/weather-forecast/static/css/main.f10b0e9d.chunk.css"
+  },
+  {
+    "revision": "279230b6bc7a42de2c51fcfb40995f2d",
+    "url": "/weather-forecast/index.html"
+  }
+];
